c1c520d2a1ebf4a3f2489f61661343d021333502
John (Adam) Stephenson <jasteph@cs.washington.edu>
2015-05-06 12:40:51 -0700

    init push

---
+++ LolNexusScraperTests/Program.cs
@@ -0,0 +1,32 @@
+﻿using LolNexusScraper.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolNexusScraperTests
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using(WebClient client = new WebClient())
+            {
+                var json = new WebClient().DownloadString("http://www.lolnexus.com/ajax/get-game-info/NA.json?name=DoctorBrowne");
+            }
+
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            LolNexusMatch match = new LolNexusMatch("NA", "Kayvinh");
+            watch.Stop();
+
+            Console.Out.WriteLine(watch.ElapsedMilliseconds);
+
+        }
+    }
+}
---
+++ WriteToImage/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteToImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Image origImage = Image.FromFile(@"C:\Users\Adam\Pictures\ScrapedMasteries\mastery_offense_bg.jpg"))
+            {
+
+                using (Bitmap newImage = new Bitmap(origImage.Width, origImage.Height))
+                {
+                    RectangleF rectf = new RectangleF(85, 480, 200, 200);
+                    Rectangle destination = new Rectangle(0, 0, origImage.Width, origImage.Height);
+                    using (Graphics g = Graphics.FromImage(newImage))
+                    {
+                        g.DrawImage(origImage, destination);
+
+                        g.SmoothingMode = SmoothingMode.AntiAlias;
+                        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        g.DrawString("30 Offense", new Font("Arial", 14), Brushes.White, rectf);
+
+                        g.Flush();
+                    }
+
+                    newImage.Save(@"C:\Users\Adam\Pictures\ScrapedMasteries\mastery_offense_bg_tmp.jpg", ImageFormat.Jpeg);
+                }
+            }
+
+
+
+        }
+    }
+}
